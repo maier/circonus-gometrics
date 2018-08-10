216dc1486404078e9a6e4aaa9f9b6e8fc6dadb06
matt maier <maier@users.noreply.github.com>
2018-08-10 11:26:19 -0400

    upd: use stream tags (|ST[...]) format for metric names when using metric deny list

--- metrics.go
+++ metrics.go
@@ -4,12 +4,33 @@
 
 package circonusgometrics
 
-// SetMetricTags sets the tags for the named metric and flags a check update is needed
+import "strings"
+
+// SetMetricTags DEPRECATED sets the tags for the named metric and flags a check update is needed
 func (m *CirconusMetrics) SetMetricTags(name string, tags []string) bool {
+	if m.check.UsingDenyList() {
+		m.Log.Println("circonusgometrics.SetMetricTags DEPRECATED - use stream tags metric_name|ST[a=val,b=val,...] format")
+		delete(m.metricTags, name)
+		return m.AddMetricTags(name, tags)
+	}
 	return m.check.AddMetricTags(name, tags, false)
 }
 
-// AddMetricTags appends tags to any existing tags for the named metric and flags a check update is needed
+// AddMetricTags DEPRECATED appends tags to any existing tags for the named metric and flags a check update is needed
 func (m *CirconusMetrics) AddMetricTags(name string, tags []string) bool {
+	if m.check.UsingDenyList() {
+		m.Log.Println("circonusgometrics.AddMetricTags DEPRECATED - use stream tags in name: metric_name|ST[a=val,b=val,...]")
+		if _, exists := m.metricTags[name]; !exists {
+			m.metricTags[name] = map[string]string{}
+		}
+		for _, tag := range tags {
+			parts := strings.Split(tag, ":")
+			if len(parts) > 2 {
+				return false
+			}
+			m.metricTags[name][parts[0]] = parts[1]
+		}
+		return true
+	}
 	return m.check.AddMetricTags(name, tags, true)
 }
